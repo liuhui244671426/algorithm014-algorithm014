08b356db30d54e903394a4677f47acb466d2ddf8
liuhui9 <liuhui9@staff.weibo.com>
2020-08-12 19:10:38 +0800

    第三天,两数之和

---
+++ Week_01/day3.go
@@ -0,0 +1,26 @@
+func twoSum(nums []int, target int) []int {
+	
+	leng := len(nums)
+	var ret []int
+	for i := 0; i < leng; i++ {
+		if nums[i] == target && target != 0 {
+			continue
+		}
+		new_target := target - nums[i]
+		res := helper_twoSum(nums, i+1, new_target)
+		if res != 0 {
+			ret = append(ret, i)
+			ret = append(ret, res)
+		}
+	}
+	return ret
+}
+
+func helper_twoSum(nums []int, start int, target int) int {
+	for i := start; i < len(nums); i++ {
+		if nums[i] == target {
+			return i
+		}
+	}
+	return 0
+}	
