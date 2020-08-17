b469acae65fca03eb1161d55e28c5f6c2704ef01
liuhui9 <liuhui9@staff.weibo.com>
2020-08-17 16:59:37 +0800

    移动零

--- Week_01/moveZeres.go
+++ Week_01/moveZeres.go
@@ -2,15 +2,12 @@
 //双指针法
 //https://leetcode-cn.com/problems/move-zeroes/submissions/
 
-func moveZeroes(nums []int) {
-	//fmt.Println(nums)
-	j := 0
-	for i := 0; i < len(nums); i++ {
-		if nums[i] != 0 {
-			nums[i], nums[j] = nums[j], nums[i]
-			j++
-		}
-		//fmt.Println(nums, j, i)
-	}
-	//fmt.Println(nums)
-}
+// func moveZeroes(nums []int) {
+// 	j := 0
+// 	for i := 0; i < len(nums); i++ {
+// 		if nums[i] != 0 {
+// 			nums[i], nums[j] = nums[j], nums[i]
+// 			j++
+// 		}
+// 	}
+// }
