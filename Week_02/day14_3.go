e1821caacb09ad917f1add79ab22031ff68e2233
liuhui9 <liuhui9@staff.weibo.com>
2020-08-23 23:43:56 +0800

    中序遍历

---
+++ Week_02/day14_3.go
@@ -0,0 +1,32 @@
+package Week_02
+
+//https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+
+//左-根-右
+//中序遍历,左链入栈
+func inorderTraversal(root *TreeNode) []int {
+	var stack []*TreeNode
+	var result []int
+
+	for len(stack) > 0 || root != nil {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		top_idx := len(stack) - 1
+		result = append(result, stack[top_idx].Val)
+		root = stack[top_idx].Right
+		stack = stack[:top_idx]
+	}
+	return result
+}
