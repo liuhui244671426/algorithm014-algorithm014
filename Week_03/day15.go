70e770fc8bc9f39fc5e8a6a650c410f805d3dd39
liuhui9 <liuhui9@staff.weibo.com>
2020-08-24 17:29:18 +0800

    二叉树中序遍历

---
+++ Week_03/day15.go
@@ -0,0 +1,25 @@
+package Week_03
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	var stack []*TreeNode
+	var result []int
+
+	for root != nil || len(stack) > 0 {
+		if root != nil {
+			stack = append(stack, root)
+			root = root.Left //取左子树,进行到底
+		} else {
+			top := stack[len(stack)-1]       //栈顶
+			result = append(result, top.Val) //出栈
+			root = top.Right                 //右子树
+			stack = stack[:len(stack)-1]     //出栈
+		}
+	}
+	return result
+}
