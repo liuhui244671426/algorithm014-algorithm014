782f429515417f7c00742c91dcef89597293099b
liuhui9 <liuhui9@staff.weibo.com>
2020-09-01 14:55:18 +0800

    子集

--- Week_04/78.go
+++ Week_04/78.go
@@ -6,7 +6,7 @@ func Subsets(nums []int) [][]int {
 
 func subsets(nums []int) [][]int {
 	path := make([]int, 0)
-	var result [][]int
+	var result [][]int //全局变量
 
 	backtrack(0, nums, path, &result)
 	return result
@@ -14,12 +14,12 @@ func subsets(nums []int) [][]int {
 
 func backtrack(cur int, nums []int, tmp []int, result *[][]int) {
 	item := make([]int, len(tmp))
-	copy(item, tmp)
+	copy(item, tmp) //复制全新一份
 	*result = append(*result, item)
 
 	for j := cur; j < len(nums); j++ {
-		tmp = append(tmp, nums[j])
-		backtrack(j+1, nums, tmp, result)
-		tmp = tmp[:len(tmp)-1]
+		tmp = append(tmp, nums[j])        //做选择
+		backtrack(j+1, nums, tmp, result) //下一个梦境
+		tmp = tmp[:len(tmp)-1]            //撤销选择
 	}
 }
