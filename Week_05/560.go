0cc5d8b449b7fe0688c8bd5ec5becd1d18690559
liuhui9 <liuhui9@staff.weibo.com>
2020-09-08 18:10:53 +0800

    和为K的子数组

---
+++ Week_05/560.go
@@ -0,0 +1,29 @@
+package Week_05
+
+import "fmt"
+
+//
+
+func SubarraySum() int {
+	var nums []int = []int{-1, -1, 1}
+	var k int = 0
+	return subarraySum(nums, k)
+}
+
+//前缀和
+//重点:[pre-k]
+func subarraySum(nums []int, k int) int {
+	count, pre := 0, 0
+	m := map[int]int{}
+	m[0] = 1 //对于下标为 0 的元素，前缀和为 0，个数为 1
+	for i := 0; i < len(nums); i++ {
+		pre += nums[i]
+
+		if _, ok := m[pre-k]; ok {
+			count += m[pre-k]
+		}
+		m[pre] += 1
+		fmt.Println("pre, m, count, pre-k", pre, m, count, pre-k)
+	}
+	return count
+}
