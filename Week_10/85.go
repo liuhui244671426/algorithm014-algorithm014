457a157595de714e0db74f1cc8bf92601ec2713c
liuhui9 <liuhui9@staff.weibo.com>
2020-10-22 10:48:24 +0800

    最大矩形

--- Week_10/85.go
+++ Week_10/85.go
@@ -1,56 +1,21 @@
 package Week_10
 
-import "fmt"
-
-//84. 柱状图中最大的矩形
-
-func LargestRectangleArea() int {
-	var heights []int
-	heights = []int{2, 1, 5, 6, 2, 3}
-	heights = []int{1, 1}
-	return largestRectangleArea(heights)
-}
+//85. 最大矩形
 
 /*
-参考代码模板
-#单调栈
-stack<int> st;
-for(int i = 0; i < nums.size(); i++)
-{
-	while(!st.empty() && st.top() > nums[i])
-	{
-		st.pop();
-	}
-	st.push(nums[i]);
-}
-
+解法:可以从 84 题,演变过来.84 使用了单调栈,计算左右边界
 */
-func largestRectangleArea(heights []int) int {
-	var ans int = 0
-	var stack []int //递增栈,存放下标
-	var top int = 0 //栈顶元素
-	// var left int = 0  //左边界
-	// var right int = 0 //右边界,区间 (right - left + 1)
-	heights = append([]int{0}, heights...)
-	heights = append(heights, 0)
-
-	for i := 0; i < len(heights); i++ {
-		for len(stack) > 0 && heights[stack[len(stack)-1]] > heights[i] {
-			top = stack[len(stack)-1]
-			stack = stack[:len(stack)-1] //pop
-			//宽度 = i - stack[end_index] - 1 ==> right-left
-			ans = max(ans, (i-stack[len(stack)-1]-1)*heights[top])
-		}
-
-		stack = append(stack, i) //push,这里存放下标
+func MaximalRectangle() int {
+	var matrix [][]byte
+	matrix = [][]byte{
+		{'1', '0', '1', '0', '0'},
+		{'1', '0', '1', '1', '1'},
+		{'1', '1', '1', '1', '1'},
+		{'1', '0', '0', '1', '0'},
 	}
-	fmt.Println(stack, ans)
-	return ans
+	return maximalRectangle(matrix)
 }
 
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
+func maximalRectangle(matrix [][]byte) int {
+	return 0
 }
