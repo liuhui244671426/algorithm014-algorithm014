fe02742071fc6d4585d715f920df5540bac52508
liuhui9 <liuhui9@staff.weibo.com>
2020-10-26 18:03:24 +0800

    有多少小于当前数字的数字

---
+++ Week_11/1365.go
@@ -0,0 +1,28 @@
+package Week_11
+
+//1365. 有多少小于当前数字的数字
+
+func SmallerNumbersThanCurrent() []int {
+	var nums []int
+	nums = []int{8, 1, 2, 2, 3}
+	return smallerNumbersThanCurrent(nums)
+}
+
+func smallerNumbersThanCurrent(nums []int) []int {
+	var res []int
+
+	for i := 0; i < len(nums); i++ {
+		minCnt := 0
+		for j := 0; j < len(nums); j++ {
+			if i == j {
+				continue //不比较自己
+			}
+			if nums[i] > nums[j] {
+				minCnt++
+			}
+		}
+		res = append(res, minCnt)
+	}
+	//fmt.Println(res)
+	return res
+}
