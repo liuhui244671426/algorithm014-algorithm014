f4a824fa6a4e5fbefcc215a0dbf03b441e38cf1b
liuhui9 <liuhui9@staff.weibo.com>
2020-11-17 11:09:05 +0800

    跳跃游戏

---
+++ Week_14/55.go
@@ -0,0 +1,29 @@
+package Week_14
+
+import "fmt"
+
+//55.跳跃游戏
+
+func CanJump() bool {
+	var nums []int = []int{
+		// 2, 3, 1, 1, 4,
+		3, 2, 1, 0, 4,
+	}
+
+	return canJump(nums)
+}
+
+func canJump(nums []int) bool {
+	var cover int = 0
+	if len(nums) == 1 {
+		return true
+	}
+	for i := 0; i <= cover; i++ {
+		cover = max(i+nums[i], cover)
+		//fmt.Println(cover, i+nums[i])
+		if cover >= len(nums)-1 {
+			return true
+		}
+	}
+	return false
+}
