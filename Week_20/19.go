df135b5e35fb0e48b1731045a47bad9f835f9758
liuhui244671426 <liuhui244671426@126.com>
2020-12-30 15:38:52 +0800

    删除链表的倒数第N个节点

--- Week_20/19.go
+++ Week_20/19.go
@@ -28,12 +28,13 @@ func RemoveNthFromEnd() *ListNode {
 //快慢指针
 //fast和 slow 相差 n 的距离
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
-	dummy := &ListNode{Val: 0, Next: head}
+	dummy := &ListNode{Val: 0, Next: head} //补充一个头指针
 	fast, slow := head, dummy
-	for i := 0; i < n; i++ {
+	//fast指针先移动 n 位,
+	for i := 0; i < n-1; i++ {
 		fast = fast.Next
 	}
-
+	//fast slow 同时移动
 	for ; fast != nil; fast = fast.Next {
 		slow = slow.Next
 	}
