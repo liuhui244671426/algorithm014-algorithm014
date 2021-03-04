c31932ef41a8e0d028dc1cabbf1aaa1bd8d99eae
liuhui244671426 <liuhui244671426@126.com>
2021-03-04 18:53:50 +0800

    比特位计数

--- Week_21/338.go
+++ Week_21/338.go
@@ -5,7 +5,7 @@ package Week_21
 给定一个非负整数 num。对于 0 ≤ i ≤ num 范围中的每个数字 i ，计算其二进制数中的 1 的数目并将它们作为数组返回。
 */
 func CountBits() []int {
-	return countBits(5)
+	return countBits2(5)
 }
 
 //牛逼
@@ -17,6 +17,16 @@ func countBits(num int) []int {
 		} else {
 			res[i] = res[i/2]
 		}
+	}
+	return res
+}
+
+//dp+位操作
+func countBits2(num int) []int {
+	var res []int = make([]int, num+1)
+	res[0] = 0
+	for i := 1; i <= num; i++ {
+		res[i] = res[i>>1] + (i & 1)
 	}
 	return res
 }
